5a868fc12ce7bf4ef0dcdc823ee7f4b893fc98bc
acolpitts <adam@completeperspective.com>
2021-05-13 22:14:32 -0300

    feature: allow CORS

--- backend/server/index.js
+++ backend/server/index.js
@@ -22,6 +22,17 @@ const app = express();
 // connect mongo db
 db.connect();
 
+// allow CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
